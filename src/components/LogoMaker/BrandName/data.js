e0fdbdabc4809f3163d3bc8f02ad647d3f5ac687
ayazsaifi <mohd.ayaz@zyloto.com>
2019-12-06 13:54:15 +0530

    clean master

--- src/components/LogoMaker/BrandName/data.js
+++
@@ -1,5 +0,0 @@
-const data = () => ({
-    logoText : JSON.parse(window.localStorage.getItem('localStorage')|| "{}").companyName
-}) ; 
-
-export default data ; 
--- src/components/LogoMaker/BrandName/methods.js
+++
@@ -1,9 +0,0 @@
-const methods = {
-    saveText() {
-        let companyName = this.logoText;
-        const localStorageData = JSON.parse(window.localStorage.getItem('localStorage') || "{}");
-        window.localStorage.setItem('localStorage', JSON.stringify({...localStorageData, companyName}));
-    }
-}
-
-export default methods
