26a783d9e60d943872339b6e609159e9f7b78449
decabitsdev-zyloto <dev@zyloto.com>
2020-05-01 19:31:38 +0530

    Landing Page changes, new logos and content

--- src/components/CreateLogo/EditLogo/index.vue
+++ src/components/CreateLogo/EditLogo/index.vue
@@ -1,60 +1,86 @@
 <template>
   <v-container>
-    <v-row class="form">
+    <v-container>
+      <v-row class="form">
         <v-col md="10" class="form__title">
-            <h2 v-if="!logoArray.length">{{cms.edit.firstTitle}}</h2>
-            <h2 v-else>{{cms.edit.title}}</h2>
+          <h2 v-if="!logoArray.length">{{ cms.edit.firstTitle }}</h2>
+          <h2 v-else>{{ cms.edit.title }}</h2>
         </v-col>
         <v-col md="2">
-            <v-btn class="form__button-new-logo" @click="saveLogoObject">New Logo</v-btn>
+          <v-btn class="form__button-new-logo" @click="saveLogoObject"
+            >New Logo</v-btn
+          >
         </v-col>
-        <v-col class="form__cards-container col-12" md="4" v-for="(item, i) in logoArray" :key="i">
-            <v-card flat outlined class="form__cards-container__card" @click="updateLogo(i)">
+        <v-col
+          class="form__cards-container col-12"
+          md="4"
+          v-for="(item, i) in logoArray"
+          :key="i"
+        >
+          <v-card
+            flat
+            outlined
+            class="form__cards-container__card"
+            @click="updateLogo(i)"
+          >
             <v-list-item>
-                <v-list-item-avatar class="form__cards-container__card-serial">{{item.id}}</v-list-item-avatar>
+              <v-list-item-avatar class="form__cards-container__card-serial">{{
+                item.id
+              }}</v-list-item-avatar>
             </v-list-item>
             <v-list-item class="pa-0">
-                <div class="form__cards-container__card__preview pa-0 ma-0" :style="{'background-color' : item.bgColor}">
-                <img :src="item.pngUrl" alt="">
-                </div>
+              <div
+                class="form__cards-container__card__preview pa-0 ma-0"
+                :style="{ 'background-color': item.bgColor }"
+              >
+                <img :src="item.pngUrl" alt="" />
+              </div>
             </v-list-item>
-            </v-card>
+          </v-card>
         </v-col>
-    </v-row>
+      </v-row>
+    </v-container>
+
+    <v-container class="firstTimeVisitTitle">
+      <v-row>
+        <v-col v-if="!logoArray.length">
+          <h2>{{ cms.edit.firstTimeVisitTitle }}</h2>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-container>
 </template>
 
 <script>
-  import Logo from '../../../models/logo'
-  import Storage from '../../../services/storage'
-  import cms from '../../../common/data/messages.json'
+import Logo from "../../../models/logo";
+import Storage from "../../../services/storage";
+import cms from "../../../common/data/messages.json";
 
-  const storage = new Storage()
+const storage = new Storage();
 
-  export default { 
-    data: () => ({
-      logoArray: storage.getAll(),
-      cms :  cms
-    }),
-    methods: {
-        saveLogoObject() {
-            const logo =  Logo.instance
-            Logo.instance = new Logo({});
-            logo.name = '';
-            logo.save();
-            this.$router.push({path:'/form'})
-        },
-        updateLogo(i) {
-        const logoSelected = this.logoArray[i]
-        storage.set(logoSelected)
-        Logo.instance = new Logo(storage.get())
-        this.$router.push({path:'/logo-maker'})
-        }
-    }
-  }
-
+export default {
+  data: () => ({
+    logoArray: storage.getAll(),
+    cms: cms,
+  }),
+  methods: {
+    saveLogoObject() {
+      const logo = Logo.instance;
+      Logo.instance = new Logo({});
+      logo.name = "";
+      logo.save();
+      this.$router.push({ path: "/form" });
+    },
+    updateLogo(i) {
+      const logoSelected = this.logoArray[i];
+      storage.set(logoSelected);
+      Logo.instance = new Logo(storage.get());
+      this.$router.push({ path: "/logo-maker" });
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-    @import './edit.scss'
+@import "./edit.scss";
 </style>
