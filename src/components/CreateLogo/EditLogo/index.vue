5655947f07b39d50e5b013aa9c52fce25b435787
Karan <guitaristkaran@gmail.com>
2020-06-12 17:00:28 +0530

    added waves in new logo section and assets issue resolved

--- src/components/CreateLogo/EditLogo/index.vue
+++ src/components/CreateLogo/EditLogo/index.vue
@@ -116,8 +116,33 @@
           <h5>Mail us with your business name at</h5>
           <p style="margin-top:2px;">info@decabits.com</p>
         </div>
+
+
+           
       </div>
+      
     </div>
+     
+
+      <!--Waves Container-->
+      <div style="position: absolute;
+    width: 100%;
+    bottom: 0;
+    left: 0;">
+      <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+      viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+      <defs>
+      <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+      </defs>
+      <g class="parallax">
+      <use xlink:href="#gentle-wave" x="48" y="0" fill="rgba(254,175,49,0.7)" />
+      <use xlink:href="#gentle-wave" x="48" y="3" fill="rgba(254,175,49,0.5)" />
+      <use xlink:href="#gentle-wave" x="48" y="5" fill="rgba(254,175,49,0.3)" />
+      <use xlink:href="#gentle-wave" x="48" y="7" fill="#feaf31" />
+      </g>
+      </svg>
+      </div>
+      <!--Waves end-->
   </v-container>
 </template>
 
@@ -209,4 +234,58 @@ export default {
 
 <style lang="scss" scoped>
 @import "./edit.scss";
+
+
+
+.waves {
+  position:relative;
+  width: 100%;
+  height:70vh;
+  margin-bottom:-7px; /*Fix for safari gap*/
+  min-height:100px;
+  max-height:200px;
+  opacity: 0.25;
+}
+
+
+/* Animation */
+
+.parallax > use {
+  animation: move-forever 25s cubic-bezier(.55,.5,.45,.5)     infinite;
+}
+.parallax > use:nth-child(1) {
+  animation-delay: -2s;
+  animation-duration: 7s;
+}
+.parallax > use:nth-child(2) {
+  animation-delay: -3s;
+  animation-duration: 10s;
+}
+.parallax > use:nth-child(3) {
+  animation-delay: -4s;
+  animation-duration: 13s;
+}
+.parallax > use:nth-child(4) {
+  animation-delay: -5s;
+  animation-duration: 20s;
+}
+@keyframes move-forever {
+  0% {
+   transform: translate3d(-90px,0,0);
+  }
+  100% { 
+    transform: translate3d(85px,0,0);
+  }
+}
+/*Shrinking for mobile*/
+@media (max-width: 768px) {
+  .waves {
+    height:40px;
+    min-height:40px;
+  }
+  .content {
+    height:30vh;
+  }
+}
+
 </style>
