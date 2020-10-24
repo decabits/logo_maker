df717d163a42a69051f34d9a9b6b70d1b4897dfa
Benson Muite <benson_muite@emailplus.org>
2020-10-24 22:11:29 +0300

    add all icons

--- src/common/data/icons.js
+++ src/common/data/icons.js
@@ -3,99 +3,509 @@ import icons from '../../components/module'
 const imageList = [
     {
       "id": 1,
-      "image": icons.ICON.cycle,
-      "imageName": "cycle"
+      "image": icons.ICON.arrow_pointing_to_left,
+      "imageName": "arrow_pointing_to_left"
     },
     {
       "id": 2,
-      "image": icons.ICON.tailor,
-      "imageName": "tailor"
+      "image": icons.ICON.arrow_pointing_to_right,
+      "imageName": "arrow_pointing_to_right"
     },
     {
       "id": 3,
-      "image": icons.ICON.cofee,
-      "imageName": "cofee"
+      "image": icons.ICON.arrow_pointing_to_up,
+      "imageName": "arrow_pointing_to_up"
     },
     {
       "id": 4,
-      "image": icons.ICON.random,
-      "imageName": "random"
+      "image": icons.ICON.arrow,
+      "imageName": "arrow"
     },
     {
       "id": 5,
-      "image": icons.ICON.bike,
-      "imageName": "bike"
+      "image": icons.ICON.barChart,
+      "imageName": "barChart"
     },
     {
       "id": 6,
-      "image": icons.ICON.travel,
-      "imageName": "travel"
+      "image": icons.ICON.battery,
+      "imageName": "battery"
     },
     {
       "id": 7,
-      "image": icons.ICON.chair,
-      "imageName": "chair"
-    },
-    {
-      "id": 8,
       "image": icons.ICON.beer,
       "imageName": "beer"
     },
     {
-      "id" : 9,
-      "image": icons.ICON.caution,
-      "imageName": "caution"
+      "id": 8,
+      "image": icons.ICON.benefitChart,
+      "imageName": "benefitChart"
+    },
+    {
+      "id": 9,
+      "image": icons.ICON.bike,
+      "imageName": "bike"
     },
     {
       "id": 10,
-      "image": icons.ICON.chain,
-      "imageName": "chain"
+      "image": icons.ICON.bookmark,
+      "imageName": "bookmark"
     },
     {
       "id": 11,
-      "image": icons.ICON.check,
-      "imageName": "check"
-    },
-    {
-      "id": 12,
-      "image": icons.ICON.envelope,
-      "imageName": "envelope"
-    },
-    {
-      "id": 13,
-      "image": icons.ICON.cocktail,
-      "imageName": "cocktail"
-    },
-    {
-      "id": 14,
-      "image": icons.ICON.compass,
-      "imageName": "compass"
-    },
-    {
-      "id": 15,
-      "image": icons.ICON.coverage,
-      "imageName": "coverage"
-    },
-    {
-      "id": 16,
       "image": icons.ICON.calendar,
       "imageName": "calendar"
     },
     {
+      "id": 12,
+      "image": icons.ICON.cancelButton,
+      "imageName": "cancelButton"
+    },
+    {
+      "id": 13,
+      "image": icons.ICON.caution,
+      "imageName": "caution"
+    },
+    {
+      "id": 14,
+      "image": icons.ICON.chain,
+      "imageName": "chain"
+    },
+    {
+      "id": 15,
+      "image": icons.ICON.chair,
+      "imageName": "chair"
+    },
+    {
+      "id": 16,
+      "image": icons.ICON.check,
+      "imageName": "check"
+    },
+    {
       "id": 17,
+      "image": icons.ICON.cloud,
+      "imageName": "cloud"
+    },
+    {
+      "id": 18,
+      "image": icons.ICON.cocktail,
+      "imageName": "cocktail"
+    },
+    {
+      "id": 19,
+      "image": icons.ICON.coffee,
+      "imageName": "coffee"
+    },
+    {
+      "id": 20,
+      "image": icons.ICON.compass,
+      "imageName": "compass"
+    },
+    {
+      "id": 21,
+      "image": icons.ICON.coverage,
+      "imageName": "coverage"
+    },
+    {
+      "id": 22,
       "image": icons.ICON.cropping,
       "imageName": "cropping"
     },
     {
-      "id": 18,
+      "id": 23,
       "image": icons.ICON.cup,
       "imageName": "cup"
     },
     {
-      "id": 19,
-      "image":icons.ICON.diamond,
+      "id": 24,
+      "image": icons.ICON.cycle,
+      "imageName": "cycle"
+    },
+    {
+      "id": 25,
+      "image": icons.ICON.diamond,
       "imageName": "diamond"
-    }
-  ]
+    },
+    {
+      "id": 26,
+      "image": icons.ICON.download_from_the_cloud,
+      "imageName": "download_from_the_cloud"
+    },
+    {
+      "id": 27,
+      "image": icons.ICON.dustbin_1,
+      "imageName": "dustbin_1"
+    },
+    {
+      "id": 28,
+      "image": icons.ICON.dustbin,
+      "imageName": "dustbin"
+    },
+    {
+      "id": 29,
+      "image": icons.ICON.edit_tool,
+      "imageName": "edit_tool"
+    },
+    {
+      "id": 30,
+      "image": icons.ICON.empty_battery,
+      "imageName": "empty_battery"
+    },
+    {
+      "id": 31,
+      "image": icons.ICON.envelope,
+      "imageName": "envelope"
+    },
+    {
+      "id": 32,
+      "image": icons.ICON.erlenmeyer_flask,
+      "imageName": "erlenmeyer_flask"
+    },
+    {
+      "id": 33,
+      "image": icons.ICON.eye,
+      "imageName": "eye"
+    },
+    {
+      "id": 34,
+      "image": icons.ICON.file_folder,
+      "imageName": "file_folder"
+    },
+    {
+      "id": 35,
+      "image": icons.ICON.full_battery,
+      "imageName": "full_battery"
+    },
+    {
+      "id": 36,
+      "image": icons.ICON.gear,
+      "imageName": "gear"
+    },
+    {
+      "id": 37,
+      "image": icons.ICON.gift_box_with_a_bow,
+      "imageName": "gift_box_with_a_bow"
+    },
+    {
+      "id": 38,
+      "image": icons.ICON.graduation_cap,
+      "imageName": "graduation_cap"
+    },
+    {
+      "id": 39,
+      "image": icons.ICON.half_full_battery,
+      "imageName": "half_full_battery"
+    },
+    {
+      "id": 40,
+      "image": icons.ICON.hand_pointing_to_left,
+      "imageName": "hand_pointing_to_left"
+    },
+    {
+      "id": 41,
+      "image": icons.ICON.hand_pointing_to_right,
+      "imageName": "hand_pointing_to_right"
+    },
+    {
+      "id": 42,
+      "image": icons.ICON.headphones,
+      "imageName": "headphones"
+    },
+    {
+      "id": 43,
+      "image": icons.ICON.heart,
+      "imageName": "heart"
+    },
+    {
+      "id": 44,
+      "image": icons.ICON.homepage,
+      "imageName": "homepage"
+    },
+    {
+      "id": 45,
+      "image": icons.ICON.hourglass,
+      "imageName": "hourglass"
+    },
+    {
+      "id": 46,
+      "image": icons.ICON.inbox,
+      "imageName": "inbox"
+    },
+    {
+      "id": 47,
+      "image": icons.ICON.international_globe,
+      "imageName": "international_globe"
+    },
+    {
+      "id": 48,
+      "image": icons.ICON.key,
+      "imageName": "key"
+    },
+    {
+      "id": 49,
+      "image": icons.ICON.laptop,
+      "imageName": "laptop"
+    },
+    {
+      "id": 50,
+      "image": icons.ICON.light_bulb,
+      "imageName": "light_bulb"
+    },
+    {
+      "id": 51,
+      "image": icons.ICON.location_flag,
+      "imageName": "location_flag"
+    },
+    {
+      "id": 52,
+      "image": icons.ICON.location_pointer,
+      "imageName": "location_pointer"
+    },
+    {
+      "id": 53,
+      "image": icons.ICON.locked_padlock,
+      "imageName": "locked_padlock"
+    },
+    {
+      "id": 54,
+      "image": icons.ICON.magic_wand,
+      "imageName": "magic_wand"
+    },
+    {
+      "id": 55,
+      "image": icons.ICON.magnifying_glass,
+      "imageName": "magnifying_glass"
+    },
+    {
+      "id": 56,
+      "image": icons.ICON.male_avatar,
+      "imageName": "male_avatar"
+    },
+    {
+      "id": 57,
+      "image": icons.ICON.medal_with_a_star,
+      "imageName": "medal_with_a_star"
+    },
+    {
+      "id": 58,
+      "image": icons.ICON.mercury_thermometer,
+      "imageName": "mercury_thermometer"
+    },
+    {
+      "id": 59,
+      "image": icons.ICON.musical_note,
+      "imageName": "musical_note"
+    },
+    {
+      "id": 60,
+      "image": icons.ICON.mute_speakers,
+      "imageName": "mute_speakers"
+    },
+    {
+      "id": 61,
+      "image": icons.ICON.notebook,
+      "imageName": "notebook"
+    },
+    {
+      "id": 62,
+      "image": icons.ICON.office_briefcase,
+      "imageName": "office_briefcase"
+    },
+    {
+      "id": 63,
+      "image": icons.ICON.old_phone,
+      "imageName": "old_phone"
+    },
+    {
+      "id": 64,
+      "image": icons.ICON.open_book,
+      "imageName": "open_book"
+    },
+    {
+      "id": 65,
+      "image": icons.ICON.open_umbrella,
+      "imageName": "open_umbrella"
+    },
+    {
+      "id": 66,
+      "image": icons.ICON.outbox_send_mail,
+      "imageName": "outbox_send_mail"
+    },
+    {
+      "id": 67,
+      "image": icons.ICON.paper_cup_with_a_drinking_straw,
+      "imageName": "paper_cup_with_a_drinking_straw"
+    },
+    {
+      "id": 68,
+      "image": icons.ICON.paper_note,
+      "imageName": "paper_note"
+    },
+    {
+      "id": 69,
+      "image": icons.ICON.photo_camera,
+      "imageName": "photo_camera"
+    },
+    {
+      "id": 70,
+      "image": icons.ICON.piece_of_paper_and_pencil,
+      "imageName": "piece_of_paper_and_pencil"
+    },
+    {
+      "id": 71,
+      "image": icons.ICON.pie_chart,
+      "imageName": "pie_chart"
+    },
+    {
+      "id": 72,
+      "image": icons.ICON.play_button,
+      "imageName": "play_button"
+    },
+    {
+      "id": 73,
+      "image": icons.ICON.power_button,
+      "imageName": "power_button"
+    },
+    {
+      "id": 74,
+      "image": icons.ICON.pushpin,
+      "imageName": "pushpin"
+    },
+    {
+      "id": 75,
+      "image": icons.ICON.question_mark,
+      "imageName": "question_mark"
+    },
+    {
+      "id": 76,
+      "image": icons.ICON.random,
+      "imageName": "random"
+    },
+    {
+      "id": 77,
+      "image": icons.ICON.reusable_shopping_bag,
+      "imageName": "reusable_shopping_bag"
+    },
+    {
+      "id": 78,
+      "image": icons.ICON.satellite_dish,
+      "imageName": "satellite_dish"
+    },
+    {
+      "id": 79,
+      "image": icons.ICON.screwdriver_and_wrench,
+      "imageName": "screwdriver_and_wrench"
+    },
+    {
+      "id": 80,
+      "image": icons.ICON.selection_tool,
+      "imageName": "selection_tool"
+    },
+    {
+      "id": 81,
+      "image": icons.ICON.shopping_basket,
+      "imageName": "shopping_basket"
+    },
+    {
+      "id": 82,
+      "image": icons.ICON.shopping_cart,
+      "imageName": "shopping_cart"
+    },
+    {
+      "id": 83,
+      "image": icons.ICON.smartphone,
+      "imageName": "smartphone"
+    },
+    {
+      "id": 84,
+      "image": icons.ICON.speech_bubble,
+      "imageName": "speech_bubble"
+    },
+    {
+      "id": 85,
+      "image": icons.ICON.speech_bubble_with_text_lines,
+      "imageName": "speech_bubble_with_text_lines"
+    },
+    {
+      "id": 86,
+      "image": icons.ICON.star,
+      "imageName": "star"
+    },
+    {
+      "id": 87,
+      "image": icons.ICON.subtracting_button,
+      "imageName": "subtracting_button"
+    },
+    {
+      "id": 88,
+      "image": icons.ICON.tablet,
+      "imageName": "tablet"
+    },
+    {
+      "id": 89,
+      "image": icons.ICON.tailor,
+      "imageName": "tailor"
+    },
+    {
+      "id": 90,
+      "image": icons.ICON.telephone_handle,
+      "imageName": "telephone_handle"
+    },
+    {
+      "id": 91,
+      "image": icons.ICON.television,
+      "imageName": "television"
+    },
+    {
+      "id": 92,
+      "image": icons.ICON.television_with_antenna,
+      "imageName": "television_with_antenna"
+    },
+    {
+      "id": 93,
+      "image": icons.ICON.thumb_up,
+      "imageName": "thumb_up"
+    },
+    {
+      "id": 94,
+      "image": icons.ICON.travel,
+      "imageName": "travel"
+    },
+    {
+      "id": 95,
+      "image": icons.ICON.unlocked_padlock,
+      "imageName": "unlocked_padlock"
+    },
+    {
+      "id": 96,
+      "image": icons.ICON.update_arrows,
+      "imageName": "update_arrows"
+    },
+    {
+      "id": 97,
+      "image": icons.ICON.update_arrow,
+      "imageName": "update_arrow"
+    },
+    {
+      "id": 98,
+      "image": icons.ICON.uploading_files_to_the_cloud,
+      "imageName": "uploading_files_to_the_cloud"
+    },
+    {
+      "id": 99,
+      "image": icons.ICON.video_camera,
+      "imageName": "video_camera"
+    },
+    {
+      "id": 100,
+      "image": icons.ICON.voice_recorder,
+      "imageName": "voice_recorder"
+    },
+    {
+      "id": 101,
+      "image": icons.ICON.volume_control,
+      "imageName": "volume_control"
+    },
+]
 
   export default imageList;
